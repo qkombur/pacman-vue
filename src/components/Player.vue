9be663626a05b0d205b235b749ba6ee561d47be5
qkombur <nick@qkombur.com>
2019-10-08 21:01:36 -0400

    boilerplate and what not dont at me

---
+++ src/components/Player.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg" 
+      height="571.11" width="541.6">
+      <path id=""d="M535.441 412.339a280.868 280.868 0 11.745-250.606L284.493 286.29z" fill="#fc0"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player',
+  props: ['xPosition, yPosition'],
+   data: function() {
+    return {
+      xPosition: 0,
+      yPosition: 0
+    }
+  },
+
+  methods: {
+
+  },
+
+  mounted: function() {
+      window.addEventListener('keydown', event => {
+        switch (event.key){
+          case "ArrowLeft":
+            console.log('left');
+            this.xPosition += 1
+            break;
+          case "ArrowRight":
+            console.log('right');
+            break;
+          case "ArrowUp":
+            console.log('up');
+            break;
+          case "ArrowDown":
+            console.log('down');
+            break;
+        }
+
+      })
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" v-on:keydown="inputHandler()">
+    <Player 
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Player from '@/components/Player.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Player
   }
+  
 }
 </script>
